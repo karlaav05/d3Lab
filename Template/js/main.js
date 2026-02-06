39ff566fa70976471affc38426734f8116e58206
karlaav05 <0251568@up.edu.mx>
2026-02-06 16:55:40 -0600

    D3 Basic - Add circle and rectangle to main.js

--- Template/js/main.js
+++ Template/js/main.js
@@ -1,4 +1,19 @@
 /*
 *    main.js
 */
+var svg = d3.select("#chart-area").append("svg")
+	.attr("width", 900)
+	.attr("height", 700);
 
+var circle = svg.append("circle")
+	.attr("cx", 700)
+	.attr("cy", 100)
+	.attr("r", 100)
+	.attr("fill", "PaleVioletRed");
+
+var rect = svg.append("rect")
+	.attr("x", 100)
+	.attr("y", 300)
+	.attr("width", 400)
+	.attr("height", 210)
+	.attr("fill","Olive");
